49e27406c7c53914dd4ebb71f88d35feb974db77
Sultan Alsawaf <sultan@kerneltoast.com>
2021-04-26 16:15:14 -0700

    simple_lmk: Skip victim reduction when all victims need to be killed
    When there aren't enough pages found, it means all of the victims that
    were found need to be killed. The additional processing that attempts to
    reduce the number of victims can be skipped in this case.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Ratoriku <a1063021545@gmail.com>

--- drivers/android/simple_lmk.c
+++ drivers/android/simple_lmk.c
@@ -144,7 +144,7 @@ static unsigned long find_victims(int *vindex)
 		 * killing the larger ones first.
 		 */
 		sort(&victims[old_vindex], *vindex - old_vindex,
-		     sizeof(*victims), victim_cmp, victim_swap);
+		     sizeof(*victims), victim_cmp, victim_swap, NULL);
 
 		/* Stop when we are out of space or have enough pages found */
 		if (*vindex == MAX_VICTIMS || pages_found >= MIN_FREE_PAGES) {
@@ -215,6 +215,7 @@ static void scan_and_kill(void)
 	if (pages_found > MIN_FREE_PAGES) {
 		/* First round of processing to weed out unneeded victims */
 		nr_to_kill = process_victims(nr_found);
+<<<<<<< HEAD
 
 		/*
 		 * Try to kill as few of the chosen victims as possible by
@@ -225,14 +226,28 @@ static void scan_and_kill(void)
 		sort(victims, nr_to_kill, sizeof(*victims), victim_cmp,
 		     victim_swap);
 
+=======
+
+		/*
+		 * Try to kill as few of the chosen victims as possible by
+		 * sorting the chosen victims by size, which means larger
+		 * victims that have a lower adj can be killed in place of
+		 * smaller victims with a high adj.
+		 */
+		sort(victims, nr_to_kill, sizeof(*victims), victim_cmp, NULL);
+
+>>>>>>> 8587d1761a81... simple_lmk: Skip victim reduction when all victims need to be killed
 		/* Second round of processing to finally select the victims */
 		nr_to_kill = process_victims(nr_to_kill);
 	} else {
 		/* Too few pages found, so all the victims need to be killed */
 		nr_to_kill = nr_found;
 	}
+<<<<<<< HEAD
 	/* Second round of victim processing to finally select the victims */
 	nr_to_kill = process_victims(nr_to_kill);
+=======
+>>>>>>> 8587d1761a81... simple_lmk: Skip victim reduction when all victims need to be killed
 
 	/* Store the final number of victims for simple_lmk_mm_freed() */
 	write_lock(&mm_free_lock);
